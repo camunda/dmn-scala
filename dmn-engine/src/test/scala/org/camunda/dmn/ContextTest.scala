3ec6eaaaf0c49d2735c3bb07349168a43f31ea05
Philipp Ossler <philipp.ossler@camunda.com>
2017-12-15 15:48:09 +0100

    add test cases for context

--- dmn-engine/src/test/scala/org/camunda/dmn/ContextTest.scala
+++ dmn-engine/src/test/scala/org/camunda/dmn/ContextTest.scala
@@ -8,6 +8,7 @@ class ContextTest extends FlatSpec with Matchers with DecisionTest {
   
   lazy val simpleContext = parse("/context/SimpleContext.dmn")
   lazy val nestedContext = parse("/context/NestedContext.dmn")
+  lazy val eligibilityContext = parse("/context/Eligibility.dmn")
     
   "A context" should "return static values" in
   {
@@ -22,13 +23,33 @@ class ContextTest extends FlatSpec with Matchers with DecisionTest {
   
   "A nested context" should "return nested values" in
   {
-    eval(simpleContext, "applicantData", Map()) should be(
+    eval(nestedContext, "applicantData", Map()) should be(
         Result(Map(
-                   "Age" -> 51,
-                   "MaritalStatus" -> "M", 
-                   "EmploymentStatus" -> "EMPLOYED", 
-                   "ExistingCustomer" -> false
+                   "EmploymentStatus" -> "EMPLOYED",
+                   "Monthly" -> Map(
+                      "Income" -> 10000.00,
+                      "Repayments" -> 2500.00,
+                      "Expenses" -> 3000.00                 
+                   )
                    ))) 
+  }
+  
+  "A context with final result" should "return only final value" in
+  {
+    val variables = Map(
+        "Applicant" -> Map(
+          "Age" -> 51,
+          "Monthly" -> Map(
+             "Income" -> 10000.00                
+          )
+        ),
+        "Affordability" -> Map(
+          "PreBureauRiskCategory" -> "DECLINE",
+          "InstallmentAffordable" -> true
+        )
+    )
+    
+    eval(eligibilityContext, "eligibility", variables) should be(Result("INELIGIBLE")) 
   }
   
 }
